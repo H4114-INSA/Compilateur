d28ad86c5fc9e35c1328efef41a56c0620da09ee
thomas <pointthomas84700@gmail.com>
2018-03-15 19:15:30 +0100

    creation Break Class

---
+++ src/Break.cpp
@@ -0,0 +1,9 @@
+//
+// Created by thomas on 15/03/18.
+//
+
+#include "Break.h"
+
+Break::Break() {}
+
+Break::~Break() {}
---
+++ src/Break.h
@@ -0,0 +1,19 @@
+//
+// Created by thomas on 15/03/18.
+//
+
+#ifndef COMPILATEUR_BREAK_H
+#define COMPILATEUR_BREAK_H
+
+
+class Break {
+public:
+    Break();
+    ~Break();
+
+private:
+
+};
+
+
+#endif //COMPILATEUR_BREAK_H
