01bacec68ff87b269a866459c425cac0c94c6f4c
thomas <pointthomas84700@gmail.com>
2018-03-15 19:15:04 +0100

    creation ExpressionAffectation Class

---
+++ src/ExpressionAffectation.h
@@ -0,0 +1,17 @@
+//
+// Created by thomas on 15/03/18.
+//
+
+#ifndef COMPILATEUR_EXPRESSIONAFFECTATION_H
+#define COMPILATEUR_EXPRESSIONAFFECTATION_H
+
+#include "ExpressionBinaire.h"
+
+class ExpressionAffectation : public ExpressionBinaire{
+
+private:
+
+};
+
+
+#endif //COMPILATEUR_EXPRESSIONAFFECTATION_H
